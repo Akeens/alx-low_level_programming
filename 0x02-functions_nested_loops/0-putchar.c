81eac7cd5728b38a0707a00cc2b7a1012f42bca1
Akeens <timmyakinyuwa@gmail.com>
2022-09-13 04:11:57 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**i
+ * main - prints the word _putchar
+ * and a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = '_putchar';
+
+	putchar("ch ");
+	putchar("\n");
+	return(0);
+}
